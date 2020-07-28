d29ed417d3863bb4ba6ab300c60093a2aaa4f685
PlayfullyBad <65961973+PlayfullyBad@users.noreply.github.com>
2020-07-28 18:00:29 +0100

    Create default.js

---
+++ default.js
@@ -0,0 +1,45 @@
+function Post(x, id){
+	id = id || "";
+	var post = {};
+	var accept = "text/plain";
+	switch(x){
+		case "SendMessage":
+			var EmailAddress = ".";
+			var EmailSubject = document.getElementById('EmailSubject').value;
+			var EmailSuggestion = document.getElementById('EmailSuggestion').value;
+			var EmailreCAPCHA = document.getElementById('reCaptureResponse').value;			
+			var EmailBody = [];
+			if(EmailSuggestion != ""){EmailBody.push("Suggestion: " + EmailSuggestion);}
+			if(EmailAddress == "" || EmailSubject == "" || EmailreCAPCHA == "" || EmailBody.length < 1){ alert("Please fill all required fields."); return; }
+			post = JSON.stringify({
+				PostType: "EmailForm",
+				EmailAddress: EmailAddress,
+				EmailSubject: EmailSubject,
+				EmailreCAPCHA: EmailreCAPCHA,
+				EmailBody: EmailBody
+			})		
+			break;
+		default:
+			return;
+	}
+	var jsonString = JSON.stringify(post);
+	const proxyurl = "https://cors-anywhere.herokuapp.com/";
+	var fullurl = proxyurl + 'https://script.google.com/macros/s/AKfycbw6DcMNV-SSPqsU000BOrxAhKBo1JCZPFUr6yAUlbNZYFVw2QU/exec';
+	fetch(fullurl, {
+		method: 'post',
+		headers: {
+			'Authorization': '',
+			'Accept': accept,
+			'Content-type': 'application/json'
+			},
+		body: post
+		})
+		.then(response=>response.text())
+		.then(function (data) {
+			alert(data);
+		})
+		.catch(function (error) {
+			alert("Failed");
+		});	
+		
+}
