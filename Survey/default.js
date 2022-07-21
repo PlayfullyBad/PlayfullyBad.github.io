0d24c413340bb2d3190c19d917ccbfac7ca69f4a
PlayfullyBad <65961973+PlayfullyBad@users.noreply.github.com>
2022-07-21 19:20:39 +0100

    Update default.js

--- Survey/default.js
+++ Survey/default.js
@@ -1,6 +1,7 @@
 const tooltipicon = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" style="fill: {{colour}};"><path d="M12 2c5.514 0 10 4.486 10 10s-4.486 10-10 10-10-4.486-10-10 4.486-10 10-10zm0-2c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2.033 16.01c.564-1.789 1.632-3.932 1.821-4.474.273-.787-.211-1.136-1.74.209l-.34-.64c1.744-1.897 5.335-2.326 4.113.613-.763 1.835-1.309 3.074-1.621 4.03-.455 1.393.694.828 1.819-.211.153.25.203.331.356.619-2.498 2.378-5.271 2.588-4.408-.146zm4.742-8.169c-.532.453-1.32.443-1.761-.022-.441-.465-.367-1.208.164-1.661.532-.453 1.32-.442 1.761.022.439.466.367 1.209-.164 1.661z"></path></svg>';
 window.onload = loadcheckURLOptions;
 
+	const proxyurl ='';// "https://cors-anywhere.herokuapp.com/";
 function initAll() {	
 	document.getElementById('jsonbuilder').innerHTML = "";	
 	document.getElementById('divDisplay').innerHTML = "";	
@@ -427,7 +428,7 @@ function createImage(){
 
 function imgurUpload(imagestring) {
 	var imgurClientId = '9db53e5936cd02f';
-	const proxyurl = "https://cors-anywhere.herokuapp.com/";
+	//const proxyurl = "https://cors-anywhere.herokuapp.com/";
 	fetch(proxyurl + 'https://api.imgur.com/3/image', {
 		method: 'post',
 		headers: {
@@ -493,7 +494,7 @@ function shareOptions(){
 	var base64Rating = window.btoa(ratingString);
 	Name = replaceAll(Name.trim()," ","");
 	var idName = ISODateString() + Name;
-	const proxyurl = "https://cors-anywhere.herokuapp.com/";
+	//const proxyurl = "https://cors-anywhere.herokuapp.com/";
 	fetch(proxyurl + 'https://script.google.com/macros/s/AKfycbztcDBVjq09BBx74UVzQ_TYxAI5cMZa7389xqBWTbL58ORmzrDa/exec', {
 		method: 'post',
 		headers: {
@@ -601,7 +602,7 @@ function loadcheckURLOptions(){
 	var myParamcol2 = "";
 	if(myParamID != "" && myParamID != null){	
 		try{
-			const proxyurl = "https://cors-anywhere.herokuapp.com/";
+			//const proxyurl = "https://cors-anywhere.herokuapp.com/";
 			var url = 'https://script.google.com/macros/s/AKfycbztcDBVjq09BBx74UVzQ_TYxAI5cMZa7389xqBWTbL58ORmzrDa/exec?id=' + myParamID;
 			fetch(url)
 				.then(response=>response.json())
